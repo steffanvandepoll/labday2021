9d5a78ba7bfc088dd4680e4748ee1ce168b18d88
Jesse van der Elst <jesse.elst@capgemini.com>
2021-12-17 11:08:41 +0100

    fgdfas

---
+++ src/components/TheThing.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="test"> 
+    <h1 class="svg"> test </h1>
+    <img src="../assets/bigHand.png" alt="">
+    </div>
+</template>
+<script>
+
+
+export default {
+    name: 'TheThing'
+}
+</script>
+<style>
+
+.test {
+    width:800px;
+  height:800px;
+  cursor:url('../assets/bigHand.png'), auto;
+}
+  
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import TheThing from '../components/TheThing.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,7 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/thething',
+    name: 'TheThing',
+    component: TheThing
+  },
 ]
 
 const router = new VueRouter({
