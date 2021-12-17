4349c379474481ff0828fdda3887b0f4d403e9c6
Steffan van de Poll <steffanvandepoll@gmail.com>
2021-12-17 11:53:03 +0100

    add a message

--- src/views/Redemption.vue
+++ src/views/Redemption.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="redemption">
-    <nav id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/hell">Hell</router-link> |
-      <router-link to="/redemption">Redemption</router-link>
-    </nav>
-    <h1>You did it!</h1>
-    <h2>I'm not even mad</h2>
+    <div class="wrapper">
+        <nav id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/hell">Hell</router-link> |
+            <router-link to="/redemption">Redemption</router-link>
+        </nav>
+        <h1>You did it!</h1>
+        <h2>I'm not even mad</h2>
+        <p>This page was not about solving puzzles... </p>
+        <blockquote>'... it's about sending a message'</blockquote>
+    </div>
   </div>
 </template>
 
@@ -18,3 +22,17 @@ export default {
   name: 'Redemption',
 }
 </script>
+
+<style scoped>
+
+ .redemption{
+   background-image: url("../assets/joker.jpg");
+   background-repeat: no-repeat;
+   background-position-y: bottom;
+   background-position-x: calc(100% + 150px);
+   display: flex;
+   height: 100vh;
+   width: 100vw;
+   justify-content: center;
+ }
+</style>
