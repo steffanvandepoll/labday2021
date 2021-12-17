4c89dfaa2a75aff87cbd8b1408fd97cac002276e
Steffan van de Poll <steffanvandepoll@gmail.com>
2021-12-17 11:25:30 +0100

    add redemption

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Redemption from "../views/Redemption.vue";
+
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/redemption",
+    name: "Redemption",
+    component: Redemption,
   },
   {
     path: "/about",
---
+++ src/views/Redemption.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="redemption">
+    <nav id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/hell">Hell</router-link> |
+      <router-link to="/redemption">Redemption</router-link>
+    </nav>
+    <h1>You did it!</h1>
+    <h2>I'm not even mad</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Redemption',
+}
+</script>
